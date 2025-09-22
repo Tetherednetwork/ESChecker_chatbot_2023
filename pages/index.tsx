9eb4d564917ce3d9eb343054ce4e72c5be760f55
Emmanuel Folarin <117568897+Tetherednetwork@users.noreply.github.com>
2025-09-22 18:55:56 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,6 @@ import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
-  useEffect(() => { router.replace("/chat"); }, [router]);
+  useEffect(() => { router.replace("/upload"); }, [router]);
   return null;
 }
